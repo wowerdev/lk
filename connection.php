e079f1d74dbb1e96db528bb459cc5f8b8421c1b8
Alex <Alex@gmail.com>
2019-07-01 16:04:48 +0300

    fix

--- connection.php
+++ connection.php
@@ -4,6 +4,8 @@ require_once $_SERVER["DOCUMENT_ROOT"] . "/config.php";
 
 $connectChar = new mysqli($server, $login, $pass, $bd_char);
 $connectAuth = new mysqli($server, $login, $pass, $bd_auth);
+$connectChar->query("SET NAMES `utf8` COLLATE `utf8_general_ci`");
+$connectAuth->query("SET NAMES `utf8` COLLATE `utf8_general_ci`");
 
 if ($connectChar->connect_errno and $connectAuth->connectAuth) {
   echo @$connectChar->connect_error . " " . @$connectAuth->connect_error;
